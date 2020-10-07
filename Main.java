c746164e4a42eaf738472c0a8ef98965c52c79a6
Isabela Ferreira <isaferreira25pires@gmail.com>
2020-10-07 14:01:45 -0300

    Commit 05
    Part 5

--- Main.java
+++ Main.java
@@ -2,8 +2,6 @@ class Main {
   public static void main(String[] args) {
     TestMultiplication tm = new TestMultiplication();
     tm.testMultiplication();
-    TestFrancMultiplication tfm= new TestFrancMultiplication();
-    tfm.testFrancMultiplication();
     TestEquality te = new TestEquality();
     te.testEquality();
     TestCurrency tc = new TestCurrency();
